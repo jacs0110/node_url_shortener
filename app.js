eb5cab0e8f5449eb312b773493e5337ee38d8934
jacs <wbruce0110@gmail.com>
2019-07-28 20:44:35 +0800

    install pg and update app.js and package.json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const flash = require('connect-flash')
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
-const port = 3000
+const port = process.env.PORT || 3000
 
 // include static files
 app.use(express.static('public'))
